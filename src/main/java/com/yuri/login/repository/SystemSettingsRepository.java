f147371f49a927676a27c6b9f9448a7ea81d72a1
YuriOnishi <yurifernandesonishi@gmail.com>
2025-06-13 14:28:28 -0300

    template

--- src/main/java/com/yuri/login/repository/SystemSettingsRepository.java
+++
@@ -1,8 +0,0 @@
-package com.yuri.login.repository;
-
-import com.yuri.login.entity.SystemSettings;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface SystemSettingsRepository extends JpaRepository<SystemSettings, Integer> {
-
-}
